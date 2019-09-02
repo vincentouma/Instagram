01ba0cf0894cdcc4c745675cc48a29187e11cd14
vincentouma <vinceoumah@gmail.com>
2019-09-02 14:30:39 +0300

    update admin

--- app/admin.py
+++ app/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
-from .models import Image,Comment,Profile
+from .models import Image,Comments,Profile
 
 # Register your models here.
 admin.site.register(Profile)
 admin.site.register(Image)
-admin.site.register(Comment)
+admin.site.register(Comments)
