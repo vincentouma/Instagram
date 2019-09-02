eba507f12cd30cb1decac149f5cfa9b0b1be7bff
vincentouma <vinceoumah@gmail.com>
2019-09-02 14:38:32 +0300

    add comment form

--- app/forms.py
+++ app/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-from .models import Image, Profile, Comment
+from .models import Image, Profile, Comments
 
 
 class SignupForm(UserCreationForm):
@@ -27,5 +27,5 @@ class ProfileForm(forms.ModelForm):
 
 class CommentForm(forms.ModelForm):
     class Meta:
-        model = Comment
+        model = Comments
         exclude = ['image', 'user']
